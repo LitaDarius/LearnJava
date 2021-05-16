a56360720ed3a68c7036d81e93cc3fd2f747f9a1
Lita Darius <darius.lita0@gmail.com>
2021-05-16 21:06:03 +0300

    empty topics can be deleted now

--- src/main/java/AdminController.java
+++ src/main/java/AdminController.java
@@ -1,3 +1,4 @@
+import javafx.collections.ObservableArray;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
@@ -17,10 +18,26 @@ public class AdminController implements Initializable {
     public void addTopic(){
         File f=new File("topics/"+topic_name.getText());
         f.mkdir();
+
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         File folder=new File("topics");
+
+        for ( File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                existent_topics.getItems().add(fileEntry.getName());    //add items to comboBox
+            }
+        }
+    }
+
+    public void delTopic(){
+        File f=new File("topics/"+existent_topics.getValue().toString());
+        if(f.delete())
+             System.out.println(existent_topics.getValue()+" was deleted");
+        else
+            System.out.println("file could not be deleted. It needs to be empty");
     }
 }
